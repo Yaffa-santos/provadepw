526eac36a89b635c5b1f520073027c29d1dd7571
Yaffa-santos <yaffasantos@gmail.com>
2024-10-30 08:37:21 -0300

    pizza adicionada amém

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Anna Clara e Yaffa Grupo</h1>
+    <img src="imagens/pizza-de-pepperoni-caseira-portal-minha-receita.jpg" alt="imagens/pizza-de-pepperoni-caseira-portal-minha-receita">
+</body>
+</html>
